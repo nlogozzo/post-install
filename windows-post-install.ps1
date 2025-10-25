59d2256e61257e1afdff075b718553e591fbe4f3
Nick Logozzo <nlogozzo225@gmail.com>
2025-10-25 13:44:43 -0400

    Update windows-post-install.ps1

--- windows-post-install.ps1
+++ windows-post-install.ps1
@@ -1,46 +1,50 @@
 function Install-Apps {
     winget source refresh
-    winget upgrade --all
-    winget install --id=Microsoft.PowerShell -e
-    winget install --id=Microsoft.Office -e
-    winget install --id=Foxit.FoxitReader -e
-    winget install --id=JRSoftware.InnoSetup -e
-    winget install --id=GitHub.GitHubDesktop -e
-    winget install --id=Microsoft.VisualStudio.2022.Community -e
-    winget install --id=Microsoft.VisualStudioCode -e
-    winget install --id=Git.Git -e
-    winget install --id Python.Python.3.12 -e
-    winget install --id=OpenJS.NodeJS -e
-    winget install --id=Zoom.Zoom -e
-    winget install --id=Discord.Discord -e
-    winget install --id=cinnyapp.cinny-desktop -e
-    winget install --id=JanDeDobbeleer.OhMyPosh -e 
-    winget install --id=mlocati.GetText -e
-    winget install --id=Fastfetch-cli.Fastfetch -e
-    winget install --id=DEVCOM.JetBrainsMonoNerdFont -e
-    winget install --id=DBBrowserForSQLite.DBBrowserForSQLite -e
-    winget install --id=DimitriVanHeesch.Doxygen -e
-    winget install --id=VideoLAN.VLC -e
-    winget install --id=Nickvision.Parabolic -e
-    winget install --id=Cyanfish.NAPS2 -e
-    winget install --id=Valve.Steam -e
-    winget install --id=EpicGames.EpicGamesLauncher -e
-    winget install --id=Cppcheck.Cppcheck -e
-    winget install --id Notepad++.Notepad++ -e
-    winget install --id Microsoft.PowerToys -e
+    sudo winget upgrade --all
+    sudo winget install --id=Microsoft.PowerShell -e
+    sudo winget install --id=Microsoft.Office -e
+    sudo winget install --id=Foxit.FoxitReader -e
+    sudo winget install --id Notepad++.Notepad++ -e
+    sudo winget install --id=Zoom.Zoom -e
+    sudo winget install --id=Discord.Discord -e
+    sudo winget install --id=cinnyapp.cinny-desktop -e
+    sudo winget install --id=JanDeDobbeleer.OhMyPosh -e 
+    sudo winget install --id=Fastfetch-cli.Fastfetch -e
+    sudo winget install --id=DEVCOM.JetBrainsMonoNerdFont -e
+    sudo winget install --id=VideoLAN.VLC -e
+    sudo winget install --id=Nickvision.Parabolic -e
+    sudo winget install --id=Cyanfish.NAPS2 -e
+    sudo winget install --id Microsoft.PowerToys -e
+    sudo winget install --id=Git.Git -e
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    $dev = Read-Host -Prompt "Install development tools? (y/n) "
+    if($dev -eq "y" -or $dev -eq "Y") { 
+		sudo winget install --id=JRSoftware.InnoSetup -e
+		sudo winget install --id=GitHub.GitHubDesktop -e
+		sudo winget install --id=Microsoft.VisualStudio.2022.Community -e
+		sudo winget install --id=Microsoft.VisualStudioCode -e
+		sudo winget install --id Python.Python.3.12 -e
+		sudo winget install --id=mlocati.GetText -e
+		sudo winget install --id=DBBrowserForSQLite.DBBrowserForSQLite -e
+		sudo winget install --id=DimitriVanHeesch.Doxygen -e
+		$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+		python -m pip install --upgrade pip
+		pip install requirements-parser
+    }
+    $games = Read-Host -Prompt "Install games? (y/n) "
+    if($games -eq "y" -or $games -eq "Y") { 
+    	sudo winget install --id=Valve.Steam -e
+    	sudo winget install --id=EpicGames.EpicGamesLauncher -e
+    }
     Start-Process https://serato.com/dj/pro/downloads
 }
 
 function Install-NotepadThemes {
     git clone "https://github.com/hellon8/VS2019-Dark-Npp"
     cd VS2019-Dark-Npp
-    sudo install.bat
+    sudo ./install.bat
     cd ..
     Remove-Item "VS2019-Dark-Npp" -Recurse -Force
-}
-
-function Install-PythonDependencies {
-    pip install requirements-parser
 }
 
 function Add-EnvironmentVariables {
@@ -58,25 +62,30 @@ function Add-PowerShellConfiguration {
 }
 
 function Install-WSL {
-    wsl --set-default-version 2
-    wsl --update --pre-release
-    wsl --install --no-distribution
-    wsl --install openSUSE-Tumbleweed
+    $install = Read-Host -Prompt "Install WSL? (y/n) "
+    if($install -eq "y" -or $install -eq "Y") { 
+        wsl --set-default-version 2
+    	wsl --update
+    	wsl --install --no-distribution
+    }
 }
 
 function Install-Vcpkg {
-    [System.Environment]::SetEnvironmentVariable("VCPKG_DEFAULT_TRIPLET","x64-windows", "User")
-    [System.Environment]::SetEnvironmentVariable("VCPKG_ROOT","C:\Users\$env:UserName\vcpkg", "User")
-    cd %HOMEPATH%
-    git clone "https://github.com/microsoft/vcpkg"
-    cd vcpkg
-    Start-Process "bootstrap-vcpkg.bat"
-    .\vcpkg.exe install boost-date-time boost-json boost-gil cpr curl gettext-libintl glfw3 gtest libjpeg-turbo libnick maddy skia[core,fontconfig,freetype,gl,harfbuzz,icu,vulkan] sqlcipher
+    $install = Read-Host -Prompt "Install vcpkg? (y/n) "
+    if($install -eq "y" -or $install -eq "Y") {
+        [System.Environment]::SetEnvironmentVariable("VCPKG_DEFAULT_TRIPLET","x64-windows", "User")
+    	[System.Environment]::SetEnvironmentVariable("VCPKG_ROOT","C:\Users\$env:UserName\vcpkg", "User")
+    	cd %HOMEPATH%
+    	git clone "https://github.com/microsoft/vcpkg"
+    	cd vcpkg
+    	Start-Process "bootstrap-vcpkg.bat"
+    	.\vcpkg.exe install boost-date-time boost-json boost-gil cpr curl gettext-libintl glfw3 gtest libjpeg-turbo libnick maddy skia[core,fontconfig,freetype,gl,harfbuzz,icu,vulkan] sqlcipher
+    }
 }
 
 function Invoke-Main {
     $ProgressPreference = 'SilentlyContinue'
-    Start-Process powershell -ArgumentList "-Command & { sudo config --enable normal }" -Verb RunAs
+    Start-Process powershell -ArgumentList "-Command & { sudo config --enable normal }" -Verb RunAs -Wait
     Install-Apps
     Install-NotepadThemes
     Install-PythonDependencies
